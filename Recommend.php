6ef468261edb627df827338cf2d832e16a057aa7
Doğan Aydın <dogan1aydin@gmail.com>
2011-04-30 16:47:28 +0300

    Şimdilik bitti gibi. Ama testlerinin yazılması lazım.

--- Recommend.php
+++ Recommend.php
@@ -113,10 +113,10 @@ class Recommendations
     /*
         return   id,type,description,url,[imageurl]
     */
-    public function alsoViewed($itemid)
+    public function alsoViewed($itemid,$n=10)
     {     
         if(!empty($itemid)) {
-            $rec = new SimpleXMLElement($this->request("/otherusersalsoviewed?itemid=42&userid=24EH1723322222A3"));
+            $rec = new SimpleXMLElement($this->request("/otherusersalsoviewed?itemid=".$itemid."&userid=".$this->userid."&numberOfResults=".$n));
             return $rec->recommendeditems->item;
         }
         else {
@@ -127,10 +127,10 @@ class Recommendations
     /*
         return   id,type,description,url,[imageurl]
     */
-    public function alsoBought($itemid)
+    public function alsoBought($itemid,$n=10)
     {
         if(!empty($itemid)) {
-            $rec = new SimpleXMLElement($this->request("/otherusersalsobought?itemid=".$itemid."&userid=".$this->userid));
+            $rec = new SimpleXMLElement($this->request("/otherusersalsobought?itemid=".$itemid."&userid=".$this->userid."&numberOfResults=".$n));
             return $rec->recommendeditems->item;
         }
         else {
@@ -141,10 +141,10 @@ class Recommendations
     /*
         return   id,type,description,url,[imageurl]
     */
-    public function ratedGood($itemid)
+    public function ratedGood($itemid,$n=10)
     {
         if(!empty($itemid)) {
-            $rec = new SimpleXMLElement($this->request("/itemsratedgoodbyotherusers?itemid=".$itemid."&userid=".$this->userid));
+            $rec = new SimpleXMLElement($this->request("/itemsratedgoodbyotherusers?itemid=".$itemid."&userid=".$this->userid."&numberOfResults=".$n));
             return $rec->recommendeditems->item;
         }
         else {
@@ -155,13 +155,13 @@ class Recommendations
     /*
         return   id,type,description,url,[imageurl]
     */
-    public function recForUser($user_id="")
+    public function recForUser($user_id="",$n=10)
     {
         if($user_id !== "") {
-            $rec = new SimpleXMLElement($this->request("/recommendationsforuser?userid=".$user_id));
+            $rec = new SimpleXMLElement($this->request("/recommendationsforuser?userid=".$user_id."&numberOfResults=".$n));
         }
         else {
-            $rec = new SimpleXMLElement($this->request("/recommendationsforuser?userid=".$this->userid));
+            $rec = new SimpleXMLElement($this->request("/recommendationsforuser?userid=".$this->userid."&numberOfResults=".$n));
         }
         return $rec->recommendeditems->item;
     }
@@ -169,10 +169,95 @@ class Recommendations
     /*
         return   id,type,description,url,[imageurl]
     */
-    public function relatedItems($itemid)
+    public function relatedItems($itemid,$n=10)
     {
-        $rec = new SimpleXMLElement($this->request("/relateditems?itemid=".$itemid."&userid=".$this->userid));
+        $rec = new SimpleXMLElement($this->request("/relateditems?itemid=".$itemid."&userid=".$this->userid."&numberOfResults=".$n));
         return $rec->recommendeditems->item;
     }
+}
+
+#doc
+#    classname:    Rankings
+#    scope:        PUBLIC
+#
+#/doc
+
+class Rankings
+{
+    #    internal variables
+    private $apikey   = "3d66b20f7cbf176bf182946a15a5378e";
+    private $tenantid = "EASYREC_DEMO";
+    private $request_url = "http://intralife.researchstudio.at:8080/api/1.0";
+
+    
+    #    Constructor
+    function __construct ($userid,$sessionid)
+    {
+        $this->userid=$userid;
+        $this->sessionid=$sessionid;        
+    }
+    
+    private function request($get_params)
+    {
+        if($get_params) {
+            $url = $this->request_url.$get_params."&apikey=".$this->apikey."&tenantid=".$this->tenantid;
+            $data = file_get_contents($url);
+            return $data;
+        }
+        else {
+            return 0;
+        }
+    }
+    
+    /*
+        return   id,type,description,url,[imageurl],[value]
+        $timeRange => DAY,WEEK,MONTH,ALL
+    */
+    public function mostViewed($timeRange="ALL",$n=10)
+    {
+        $rec = new SimpleXMLElement($this->request("/mostvieweditems?timeRange=".$timeRange."&numberOfResults=".$n));
+        return $rec->recommendeditems->item;
+    }
+    
+    /*
+        return   id,type,description,url,[imageurl],[value]
+        $timeRange => DAY,WEEK,MONTH,ALL
+    */
+    public function mostBought($timeRange="ALL",$n=10)
+    {
+        $rec = new SimpleXMLElement($this->request("/mostboughtitems?numberOfResults=".$n."&timeRange=".$timeRange));
+        return $rec->recommendeditems->item;
+    }
+    
+    /*
+        return   id,type,description,url,[imageurl],[value]
+        $timeRange => DAY,WEEK,MONTH,ALL
+    */
+    public function mostRated($timeRange="ALL",$n=10)
+    {
+        $rec = new SimpleXMLElement($this->request("/mostrateditems?numberOfResults=".$n."&timeRange=".$timeRange));
+        return $rec->recommendeditems->item;
+    }
+    
+    /*
+        return   id,type,description,url,[imageurl],[value]
+        $timeRange => DAY,WEEK,MONTH,ALL
+    */
+    public function bestRated($timeRange="ALL",$n=10)
+    {
+        $rec = new SimpleXMLElement($this->request("/bestrateditems?numberOfResults=".$n."&timeRange=".$timeRange));
+        return $rec->recommendeditems->item;
+    }
+    
+    /*
+        return   id,type,description,url,[imageurl],[value]
+        $timeRange => DAY,WEEK,MONTH,ALL
+    */
+    public function worstRated($timeRange="ALL",$n=10)
+    {
+        $rec = new SimpleXMLElement($this->request("/worstrateditems?numberOfResults=".$n."&timeRange=".$timeRange));
+        return $rec->recommendeditems->item;
+    }
+
 }
 ?>
